6bcc8473aa1362ca01f73730a7e6c2bf6241dd93
Wooseong Kim <91881810+kylewskim@users.noreply.github.com>
2022-06-23 13:52:10 +0000

    initial setting

---
+++ js/app.js
@@ -0,0 +1,10 @@
+import * as THREE from '../node_modules/three/build/three.module.js';
+
+console.log(THREE);
+
+// import Stats from '../node_modules/three/examples/jsm/libs/stats.module.js';
+// import { GLTFLoader } from '../node_modules/three/examples/jsm/loaders/GLTFLoader.js';
+// import { Octree } from '../node_modules/three/examples/jsm/math/Octree.js';
+// import { OctreeHelper } from '../node_modules/three/examples/jsm/helpers/OctreeHelper.js';
+// import { Capsule } from '../node_modules/three/examples/jsm/math/Capsule.js';
+// import { GUI } from '../node_modules/dat.gui/build/dat.gui.module.js';
